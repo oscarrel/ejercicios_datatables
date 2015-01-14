e055d654e76bbd3f963cc07c319296714bc575db
oscarrel <oscarrela@yahoo.es>
2015-01-14 13:55:37 +0100

    Ejercicio 2 solucionado

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,7 +164,7 @@ module.exports = function (grunt) {
     wiredep: {
       app: {
         ignorePath: /^\/|\.\.\//,
-        src: ['<%= config.app %>/index.html']
+        src: ['<%= config.app %>/ejercicio*.html']
       }
     },
 
@@ -190,7 +190,7 @@ module.exports = function (grunt) {
       options: {
         dest: '<%= config.dist %>'
       },
-      html: '<%= config.app %>/index.html'
+      html: '<%= config.app %>/ejercicio*.html'
     },
 
     // Performs rewrites based on rev and the useminPrepare configuration
